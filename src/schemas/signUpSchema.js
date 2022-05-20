eea1eecba5ee63fb631941a84c99e5d896972099
Luana Lessa <lessalsn@gmail.com>
2022-05-20 19:40:40 -0300

    Code review

--- src/schemas/signUpSchema.js
+++
@@ -1,9 +0,0 @@
-import joi from "joi";
-
-const signUpSchema = joi.object({
-  name: joi.string().min(2).required(),
-  email: joi.string().email().required(),
-  password: joi.string().min(3).max(15).required(),
-});
-
-export default signUpSchema;
